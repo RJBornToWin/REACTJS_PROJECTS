7ecbd9ecfa09332db7c6738d4f15c0a1c79f3a78
RJBornToWin <rajasri215@gmail.com>
2022-07-07 21:09:15 -0700

    Components creation

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@
 import './App.css';
 
 import PageHeader from './Components/PageHeader';
-import SearcheableProductData from './Components/SearcheableProductData';
+import FilterableProductData from './Components/FilterableProductData';
 import PageFooter from './Components/PageFooter';
 
 function App() {
+  const PRODUCTS = [
+    {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+    {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+    {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+    {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+    {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+    {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+  ];
+
   return (
     <div className="App">
       <PageHeader text="Searcheable Product Data"/>
-      <SearcheableProductData className="App-content"/>
+      <FilterableProductData className="App-content" products={PRODUCTS}/>
       <PageFooter text="This page was last edited on 25 June 2022, at 21:34 (UTC)." />
     </div>
   );
